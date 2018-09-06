7b42b6d27a7ffed7c748a7fdc3a226d75deae2b1
Patrick Grey <websemantic@yahoo.co.uk>
2018-09-06 14:23:31 +0100

    Got read in of json working.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,30 @@
-import React from "react";
-import Header from "../components/header";
-import Link from "gatsby-link";
+import React from 'react'
+import { Link, Graphql } from 'gatsby'
 
-export default () => (
-  <div style={{ color: `purple` }}>
-    <Header headerText="About Gatsby" />
-    <Link to="/phrase1/">phrase1</Link>
-    <p>What a world.</p>
-  </div>
-);
+import Layout from '../components/layout'
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    {data.allPhrasesJson.edges.map(({ node }) => (
+      <p key={node.phraseId}>{node.title}</p>
+    ))}
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    allPhrasesJson {
+      edges {
+        node {
+          phraseId
+          title
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
--- src/pages/phrase1.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Header from "../components/header";
-
-export default () => (
-  <div style={{ color: `teal` }}>
-    <Header headerText="Phrase 1" />
-    <p>Such wow. Very React.</p>
-  </div>
-);
